d9871aec82d212e870dcaf6eb43bcfb187a8e860
rafeezk <rayzaqqq@gmail.com>
2024-03-08 09:34:15 +0700

    4 up

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import AuthProvider from "./auth/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/warehouseproject/">
       <AuthProvider>
       <App />
       </AuthProvider>
